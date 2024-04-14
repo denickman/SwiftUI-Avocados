9d187654289877109780e50aa26df3b19f45ea21
Denis <denis.yaremenko@huma.com>
2024-04-14 13:15:23 +0300

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Denis Yaremenko on 14.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
